3ec3f9d8cb9e530cae7bad2a85ec756d6ace4a62
Diego Essaya <dessaya@gmail.com>
2021-09-18 18:49:44 -0300

    markdown

--- poll.py
+++ poll.py
@@ -4,6 +4,9 @@ import random
 import string
 import os
 import json
+import markdown
+
+md = markdown.Markdown(output_format="html5", extensions=['fenced_code', 'nl2br'])
 
 PORT = int(os.environ.get('PORT', '8000'))
 BACKUP_FILE = os.environ.get('BACKUP_FILE', '')
@@ -23,9 +26,9 @@ def link(path):
     return f'{basepath()}{path}'
 
 class Poll:
-    def __init__(self, title, options, multiple_choice):
+    def __init__(self, question, options, multiple_choice):
         self.key = make_key()
-        self.title = title
+        self.question = question
         self.options = options
         self.multiple_choice = multiple_choice
         self.responses = [0 for i in range(len(options))]
@@ -103,6 +106,7 @@ def get_poll(key):
 _GITHUB_LINK = '''<a href="https://github.com/dessaya/simplepoll" class="github-corner" aria-label="View source on GitHub"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a><style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>'''
 
 def html(s):
+    markdown_p = 'display: inline-block; margin-bottom: 0;'
     return f'''
         <!doctype html><html><head>
         <meta charset="utf-8">
@@ -115,6 +119,13 @@ def html(s):
             tbody tr:hover {{ background: #f0f0f0; }}
             table {{ width: auto; }}
             td, th {{ border: none; }}
+            form, fieldset {{ margin-bottom: 0; }}
+            form label p {{ {markdown_p} }}
+            th p {{ {markdown_p} }}
+            .question {{
+                border-bottom: 1px solid #f0f0f0;
+                margin-bottom: 1em;
+            }}
         </style>
         <title>SimplePoll</title>
         </head><body><section>
@@ -128,29 +139,36 @@ def error(status, msg):
 
 @route('/', method="GET")
 def index():
+    nl = '\n'
     return html(f'''
         <h2>Step 1: Configure your poll</h2>
         <form action="{link('/')}" method="post">
-            <fieldset style="width: 100%">
-            <input style="width: 100%" name="title" id="title" type="text" placeholder="Title" />
-            <textarea style="width: 100%" id="options" name="options" rows="10" cols="50" placeholder="Options (one per line)"></textarea>
+            <label for="question">Poll question</label>
+            <textarea style="width: 100%" name="question" id="question" rows="3" cols="50" placeholder="What is your favorite ice cream flavor?"></textarea>
+            <label for="options">Options (one per line)</label>
+            <textarea style="width: 100%" id="options" name="options" rows="5" cols="50" placeholder="Vanilla{nl}Chocolate{nl}Strawberry"></textarea>
             <label><input type="checkbox" name="multiple_choice">Multiple choice</label>
-            <input type="submit" value="Create poll">
-            </fieldset>
+            <div><label><strong>Tip:</strong> You can use <a href="https://www.markdownguide.org/basic-syntax/">Markdown</a>!</label></div>
+            <div><input type="submit" value="Create poll"></div>
         </form>
     ''')
 
+
 @route('/', method="POST")
 def create_poll():
-    title = request.forms.title.strip()
-    options = [s.strip() for s in request.forms.options.split('\n') if s.strip()]
+    question = md.reset().convert(request.forms.question.strip())
+    options = [
+        md.reset().convert(s.strip())
+        for s in request.forms.options.split('\n')
+        if s.strip()
+    ]
     multiple_choice = request.forms.multiple_choice == "on"
 
     if len(options) < 2:
         error(400, 'We need at least 2 options.')
 
     for _ in range(5):
-        poll = Poll(title, options, multiple_choice)
+        poll = Poll(question, options, multiple_choice)
         if poll.key not in polls:
             break
     else:
@@ -189,7 +207,7 @@ def poll_results(key):
     poll = get_poll(key)
     return html(template(
         '''
-            <h1>{{poll.title}}</h1>
+            <div class="question">{{!poll.question}}</div>
             <p>Responses: {{!poll.responses_count}}</p>
             <center><table>
                 <tbody>
@@ -197,7 +215,7 @@ def poll_results(key):
                     % p = poll.percentage(i)
                     % option = option if show else '???'
                     <tr>
-                        <th style="max-width: 50%; text-align: end;">{{option}}</th>
+                        <th style="max-width: 50%; text-align: end;">{{!option}}</th>
                         <td style="text-align: end; padding: 0 1em;"><nobr>{{!poll.responses[i]}} votes</nobr></td>
                         <td style="text-align: end;"><nobr>{{p}}%</nobr></td>
                         <td style="min-width: 200px"><div style="width:100%; height: 1em; border: 1px solid #007bff; background: #fff"><div style="width:{{!p}}%; height:100%; background-color: #007bff"></div></div></td>
@@ -219,13 +237,13 @@ def poll_vote_form(key):
     poll = get_poll(key)
     return html(template(
         '''
-            <h1>{{poll.title}}</h1>
+            <div class="question">{{!poll.question}}</div>
             <form action="{{!poll.vote_url()}}" method="post">
                 <fieldset>
                 % type = 'checkbox' if poll.multiple_choice else 'radio'
                 % required = '' if poll.multiple_choice else 'required'
                 % for (i, option) in enumerate(poll.options):
-                    <label><input type="{{!type}}" name="option" value="{{!i}}" {{!required}}>{{option}}</label>
+                    <label><input type="{{!type}}" name="option" value="{{!i}}" {{!required}}>{{!option}}</label>
                 % end
                 <input type="submit" value="Vote">
                 </fieldset>
@@ -238,7 +256,7 @@ def poll_vote_form(key):
 def poll_cast_vote(key):
     votes = [int(i) for i in request.forms.getall('option')]
     get_poll(key).cast_vote(votes)
-    return html('<h1>Thank you for voting!</h1>')
+    return html('<h1>Thank you for participating!</h1>')
 
 polls = OrderedDict()
 Backup.load()
